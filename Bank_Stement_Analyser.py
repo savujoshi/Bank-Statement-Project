e3915ba735ac6753751efb90bf565db0edb09535
Keval Patel <keval.patel@ucalgary.ca>
2022-10-03 02:37:22 -0600

    Version2-Statement Parsing

--- Bank_Stement_Analyser.py
+++ Bank_Stement_Analyser.py
@@ -18,17 +18,40 @@ def parse_Scotia_Pdf(filePath):
             pageObj = pdfReader.getPage(pageNo)
             statementPageLines = pageObj.extract_text().split('\n')
             for item in statementPageLines:
+                if pageNo == 0:
+                    # print(item)
+                    if "Statement Period" in item and pageNo == 0 and statementPageLines[lineCounter-8] == "SCENE":
+                        # We can get statement period here
+                        print(item)
+                        print(statementPageLines[lineCounter+1])
+                        print(statementPageLines[lineCounter+3])
+                        print("\n")
+                    if "Continued on page" in item:
+                        # We can directly jump to page number
+                        # print(item[-1])
+                        pass
+                    if "Transactions since your last statement" in item:
+                        statementNum = statementPageLines[lineCounter+12]
+                        # Found 
+                        print("Transaction Number:",statementNum)
+                        statementLineCounter = 1
+                        for i in range(lineCounter+12,len(statementPageLines)):
+                            if statementPageLines[i].isnumeric():
+                                if int(statementPageLines[lineCounter+12])+statementLineCounter == int(statementPageLines[i]):
+                                    print("\n")
+                                    for j in range(0,8):
+                                        if statementPageLines[i+j].isnumeric():
+                                            if int(statementPageLines[lineCounter+12])+statementLineCounter +1 == int(statementPageLines[i+j]):
+                                                # print(int(statementPageLines[lineCounter+12])+statementLineCounter +1)
+                                                break
+                                        print(statementPageLines[i+j])
+                                      
+                                    statementLineCounter+=1
 
-                if "Statement Period" in item and pageNo == 0 and statementPageLines[lineCounter-8] == "SCENE":
-                    # We can get statement period here
-                    print(item)
-                    print(statementPageLines[lineCounter+1])
-                    print(statementPageLines[lineCounter+3])
-                    print("\n")
-                if "Continued on page" in item:
-                    # We can directly jump to page number
-                    print(item[-1])
-                lineCounter += 1
+                        # print(int(statementNum)+1)
+                        
+                    lineCounter += 1
+
 
             print("\n \n")
     pdfFileObj.close()
