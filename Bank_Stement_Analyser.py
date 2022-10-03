3d2235142acaec9dffe2fb523e138cac7f75d805
Keval Patel <keval.patel@ucalgary.ca>
2022-10-03 03:15:26 -0600

    Space changes

--- Bank_Stement_Analyser.py
+++ Bank_Stement_Analyser.py
@@ -15,7 +15,7 @@ def parse_Scotia_Pdf(filePath):
 
     for pageNo in range(0, pdfReader.numPages):
         lineCounter = 0
-        
+
         if pageNo != 1:
             pageObj = pdfReader.getPage(pageNo)
             statementPageLines = pageObj.extract_text().split('\n')
@@ -27,66 +27,68 @@ def parse_Scotia_Pdf(filePath):
                         print(item)
                         print(statementPageLines[lineCounter+1])
                         print(statementPageLines[lineCounter+3])
-                        print("\n")
+                        # print("\n")
                     if "Continued on page" in item:
                         # We can directly jump to page number
                         # print(item[-1])
                         pass
                     if "Transactions since your last statement" in item:
                         statementNum = statementPageLines[lineCounter+12]
-                        # Found 
-                        print("Transaction Number:",statementNum)
+                        # Found
+                        # print("Transaction Number:", statementNum)
                         statementLineCounter = 0
-                        for i in range(lineCounter+12,len(statementPageLines)):
+                        for i in range(lineCounter+12, len(statementPageLines)):
                             if statementPageLines[i].isnumeric():
                                 if int(statementPageLines[lineCounter+12])+statementLineCounter == int(statementPageLines[i]):
                                     # print("\n")
                                     # print(statementPageLines[i])
                                     transactionElements = []
-                                    for j in range(0,8):
+                                    for j in range(0, 8):
                                         if statementPageLines[i+j].isnumeric():
-                                            if int(statementPageLines[lineCounter+12])+statementLineCounter +1 == int(statementPageLines[i+j]):
+                                            if int(statementPageLines[lineCounter+12])+statementLineCounter + 1 == int(statementPageLines[i+j]):
                                                 # print(int(statementPageLines[lineCounter+12])+statementLineCounter +1)
                                                 break
-                                        if j!=0:
-                                            transactionElements.append(statementPageLines[i+j])
+                                        if j != 0:
+                                            transactionElements.append(
+                                                statementPageLines[i+j])
                                         # print(statementPageLines[i+j])
                                     # print(int(statementPageLines[i]))
-                                    transactionDict.update({int(statementPageLines[i]):transactionElements})
+                                    transactionDict.update(
+                                        {int(statementPageLines[i]): transactionElements})
                                     # print(transactionDict)
-                                    statementLineCounter+=1
+                                    statementLineCounter += 1
 
                         # print(int(statementNum)+1)
-                        
+
                     lineCounter += 1
                 else:
                     # print(item)
                     if "Transactions - continued" in item:
-                        statementNum = statementPageLines[lineCounter+8]
-                        print(statementNum)
+                        # print(statementNum)
                         statementLineCounter = 0
-                        for i in range(lineCounter+8,len(statementPageLines)):
+                        for i in range(lineCounter+8, len(statementPageLines)):
                             if statementPageLines[i].isnumeric():
                                 if int(statementPageLines[lineCounter+8])+statementLineCounter == int(statementPageLines[i]):
                                     # print("\n")
                                     # print(statementPageLines[i])
                                     transactionElements = []
-                                    for j in range(0,8):
+                                    for j in range(0, 8):
                                         if statementPageLines[i+j].isnumeric():
-                                            if int(statementPageLines[lineCounter+8])+statementLineCounter +1 == int(statementPageLines[i+j]):
+                                            if int(statementPageLines[lineCounter+8])+statementLineCounter + 1 == int(statementPageLines[i+j]):
                                                 # print(int(statementPageLines[lineCounter+12])+statementLineCounter +1)
                                                 break
-                                        if j!=0:
-                                            transactionElements.append(statementPageLines[i+j])
+                                        if j != 0:
+                                            transactionElements.append(
+                                                statementPageLines[i+j])
                                         # print(statementPageLines[i+j])
                                     # print(int(statementPageLines[i]))
-                                    transactionDict.update({int(statementPageLines[i]):transactionElements})
+                                    transactionDict.update(
+                                        {int(statementPageLines[i]): transactionElements})
                                     # print(transactionDict)
-                                    statementLineCounter+=1
+                                    statementLineCounter += 1
                     lineCounter += 1
-                
 
-            print("\n \n")
+    print("\n")
     pdfFileObj.close()
     print(transactionDict)
 
