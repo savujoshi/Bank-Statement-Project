daf42be7dc3e71e55650fe67c807a88a57049f55
Keval Patel <keval.patel@ucalgary.ca>
2022-10-03 01:39:43 -0600

    -Statement Period can be extracted -Jumping to page number

--- Bank_Stement_Analyser.py
+++ Bank_Stement_Analyser.py
@@ -1,37 +1,50 @@
 from webbrowser import get
 import PyPDF2
-  
+
 
 def main_function():
-    # creating a pdf file object
-    pdfFileObj = open('Scotia_September_statement.pdf', 'rb')
-    
-    # creating a pdf reader object
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    
-    # printing number of pages in pdf file
-    print("Number of pages:"+str(pdfReader.numPages))
-    
-    # creating a page object
-    for pageNo in range(0, pdfReader.numPages):
-        print(pageNo)
-        pageObj = pdfReader.getPage(pageNo)
-        print("\n")
-        print(len(pageObj.extractText().splitlines()))
 
-    print("Object Type:" + str(type(pageObj)))
-    
-    # extracting text from page
-    # print(pageObj.extractText())
-    
-    # closing the pdf file object
+    filePathScotia = 'Scotia_September_statement.pdf'
+    parse_Scotia_Pdf(filePathScotia)
+
+
+def parse_Scotia_Pdf(filePath):
+    pdfFileObj = open(filePath, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    for pageNo in range(0, pdfReader.numPages):
+        lineCounter = 0
+        if pageNo != 1:
+            pageObj = pdfReader.getPage(pageNo)
+            statementPageLines = pageObj.extract_text().split('\n')
+            for item in statementPageLines:
+
+                if "Statement Period" in item and pageNo == 0 and statementPageLines[lineCounter-8] == "SCENE":
+                    # We can get statement period here
+                    print(item)
+                    print(statementPageLines[lineCounter+1])
+                    print(statementPageLines[lineCounter+3])
+                    print("\n")
+                if "Continued on page" in item:
+                    # We can directly jump to page number
+                    print(item[-1])
+                lineCounter += 1
+
+            print("\n \n")
     pdfFileObj.close()
 
 
-def parse_pdf():
-    pass
+def parse_Neo_Pdf(filePath):
+    pdfFileObj = open(filePath, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    for pageNo in range(0, pdfReader.numPages):
+        if pageNo != 1:
+            pageObj = pdfReader.getPage(pageNo)
+
+    pdfFileObj.close()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
     main_function()
